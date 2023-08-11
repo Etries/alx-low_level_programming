358502829be1cef5af791e9fdadc57f0c3fdec24
Etries <elyudmichael@gmail.com>
2023-08-11 12:09:39 -0700

    102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+*main - prints all possible different combination os 3 digits
+*
+*Return: will return (0) (success)
+*
+*/
+int main(void)
+{
+	int i, j, k, l;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			k = i;
+			l = j + 1;
+			while (k < 10)
+			{
+				while (l < 10)
+				{
+					putchar(i + 48);
+					putchar(j + 48);
+					putchar(' ');
+					putchar(k + 48);
+					putchar(l + 48);
+					if (i < 9 || j < 8 || k < 9 || l < 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+				l = 0;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
