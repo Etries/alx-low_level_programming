5260258a18264553765dd7b1e9b7b7d8d938104c
Etries <elyudmichael@gmail.com>
2023-08-11 05:20:21 -0700

    print_all_alphabets c file added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+*main - display all lowercase alphabets in decreasing order z-a
+*
+*Return: will return (0) (success)
+*
+*/
+int main(void)
+{
+	int small_alphabet = 'a';
+	int capital_alphabet = 'A';
+
+	while (small_alphabet <= 'z')
+	{
+		putchar(small_alphabet);
+		small_alphabet++;
+	}
+	while (capital_alphabet <= 'Z')
+	{
+		putchar(capital_alphabet);
+		capital_alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
