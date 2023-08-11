ea23e718a3aafacbcd992e897287f0c605fc415c
Etries <elyudmichael@gmail.com>
2023-08-11 05:31:04 -0700

    print_alphabt.c file added

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+*main - display all lowercase alphabets in decreasing order z-a
+*
+*Return: will return (0) (success)
+*
+*/
+int main(void)
+{
+	int alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet == 'e' || alphabet == 'e')
+		{
+			alphabet++;
+			continue;
+		}
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
