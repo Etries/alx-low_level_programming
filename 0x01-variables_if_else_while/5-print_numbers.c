e2194122fb8136575d17ead251130bba6ba45624
Etries <elyudmichael@gmail.com>
2023-08-11 06:10:57 -0700

    5-print_numbers c file added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - display all lowercase alphabets in decreasing order z-a
+*
+*Return: will return (0) (success)
+*
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
