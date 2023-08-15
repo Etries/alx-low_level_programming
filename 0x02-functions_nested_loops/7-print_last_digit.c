5bb865664db089b69f52eb98a656443caa9e00c6
Etries <elyudmichael@gmail.com>
2023-08-15 13:53:05 -0700

    7-print_last_digit.c file addedwq

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - finds the last digit by modulus
+*
+*@n: the number where we find the last digit of
+* Return: the last digit
+*
+*/
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
