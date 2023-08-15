f9d92c4d15785381746d3c512c84bcdc181c018c
Etries <elyudmichael@gmail.com>
2023-08-15 14:10:28 -0700

    8-24_hours.c file added

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* jack_bauer - finds the last digit by modulus
+*
+* Return: Nothing
+*
+*/
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(':');
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
+
+
