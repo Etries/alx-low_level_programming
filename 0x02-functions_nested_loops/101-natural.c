cfe3b8833565e56b91c43258d3a78f9228559b3f
Etries <elyudmichael@gmail.com>
2023-08-16 08:13:02 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - adds all numbers that are multiple of 3 and 5
+ *
+ *Return:  Always success (0)
+ *
+ */
+int main(void)
+{
+	int i, sum;
+
+	sum = 0;
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sum);
+}
