3bb278b5ebcd85ce61f0bebf01b3c9ff293b876f
Etries <elyudmichael@gmail.com>
2023-08-16 00:26:56 -0700

    9-times_table.c addedWq

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,35 @@
 #include "main.h"
 /**
 * times_table - displays a multiplication table for 9
-* 
-*@n: the number where we find the last digit of
+*
 * Return: Nothing
 *
 */
 void times_table(void)
 {
-	int i, j;
+	int i, j, num;
 
+	for (i = 0; i <= 9 ; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
 
+			num = j * i;
+			if (num < 10)
+			{
+				_putchar(num + 48);
+			}
+			else
+			{
+				_putchar(num / 10 + 48);
+				_putchar(num % 10 + 48);
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
