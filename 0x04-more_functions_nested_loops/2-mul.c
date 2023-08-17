51476afddb9815df895c3a327ede63025f131df0
Etries <elyudmichael@gmail.com>
2023-08-17 03:12:09 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - takes two numbers as argument n retirs thier multiplication
+ * @a : first number
+ * @b : second number to be multiplied
+ *
+ * Return: returns the result of the two numbers multiplied
+ *
+ */
+int mul(int a, int b);
+{
+	return (a * b);
+}
