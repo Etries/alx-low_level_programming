4cb0d1f10dd12352d94833ec39346fe3fa60b9c8
Etries <elyudmichael@gmail.com>
2023-08-18 13:56:11 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+*main : prints a fiz if multiple of 3, buzz multiple of 5 n fizzbuzz multiple of both 
+*
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	int num;
+	while (num++ < 100)
+
+	if ((num % 15 == 0))
+	printf("FizzBuzz ");
+
+	else if ((num % 3) == 0)
+	printf("Fizz ");
+
+	else if ((num % 5) == 0)
+	{
+		if (num != 100)
+		printf("Buzz ");
+
+	else
+		printf("Buzz");
+	}
+
+	else
+		printf("%d ", num);
+
+	printf("\n");
+	return (0);
+}
