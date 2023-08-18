e7943c965c5b8420ed811826faf772302363d8d5
Etries <elyudmichael@gmail.com>
2023-08-18 13:50:33 -0700

    8-print_square.c edited

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_sqaure - displays a square followed by a new line.
+ * print_square - displays a square followed by a new line.
  *@size : the size of the sqaue to be made
  * Return  : nothing
  *
@@ -13,7 +13,7 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			for (j =1; j < size; j++)
+			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
