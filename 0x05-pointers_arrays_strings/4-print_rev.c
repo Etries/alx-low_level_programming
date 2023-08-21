ab4e4e4903a3402d20a590adedc14216ec218147
Etries <elyudmichael@gmail.com>
2023-08-21 14:06:32 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s : the string to be reversed
+ *
+ * Return : nothing
+ *
+ */
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	for (j = i; j > 0; j--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
