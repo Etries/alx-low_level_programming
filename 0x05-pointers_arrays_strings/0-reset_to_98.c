8f6fa7ee4a7e609dde0520068682b3cd0a142461
Etries <elyudmichael@gmail.com>
2023-08-21 04:52:39 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "maih.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n : intger to be set
+ * Return n nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = oi;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+fndef MAIN_HH
+#define MAIN_H 
+void reset_to_98(int *n);i
+
+#endif
