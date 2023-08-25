acb34ef1442e683d872b673ed4d95a9606254339
Etries <elyudmichael@gmail.com>
2023-08-24 17:08:46 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * reverse_array - reverses the content of an array
+ * @a: array to be reversed
+ * @n number of elements to be swapped
+ * Return: nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, i;
+
+	n = n - 1;
+	for (i = n ; i > (n + 1) / 2; i--)
+	{
+	tmp = a[n - i];
+	a[n - i] = a[i];
+	a[i] = tmp;
+	}
+}
