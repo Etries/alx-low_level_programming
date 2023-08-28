d96df3462cb3cd3a94d539cfd81df575968d0547
Etries <elyudmichael@gmail.com>
2023-08-28 15:44:23 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s : string to be searched
+ * @c : character to be searched
+ * Return: returns a pointer to a char in the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+}
