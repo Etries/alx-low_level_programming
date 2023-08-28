c86bd56743fbfc6fcfeede2267d148f2b6fdf69a
Etries <elyudmichael@gmail.com>
2023-08-28 16:31:46 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,6 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strbrk(char *s, char * accept);
 char *strstr(char *heystack, char *needle);
+void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif
