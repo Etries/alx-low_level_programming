773c2cffc8691006c458358d9f46dfa717a14d21
Etries <elyudmichael@gmail.com>
2023-08-29 15:20:10 -0700

    100-set_string.c

---
+++ 0x06-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * ser_string - sets the value of a pointer to a char
+ * @s : pointer to address of string
+ * @to :  pointer to the value
+ *
+ * Return: returns nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
