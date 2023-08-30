5a4d3bc5abc5288bc0028ace529138e4ebcd65c0
Etries <elyudmichael@gmail.com>
2023-08-30 16:09:42 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _prints_rev_recursion - prints string in reverse
+ * _print_rev_recursion - prints string in reverse
  * @s : dtring to be reversed
  *
  * Return : returns nothing
  */
-void _prints_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_prints_rev_string(s + 1);
+		_print_rev_string(s + 1);
 		_putchar(*s);
 	}
 	else
-		putchar('\n');
+		_putchar('\n');
 }
