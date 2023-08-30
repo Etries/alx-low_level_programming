bc82123db34c168cd22e7e8770294ba950829359
Etries <elyudmichael@gmail.com>
2023-08-30 14:22:40 -0700

    intital commit

---
+++ 0x08-recursion/1-pront_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "mian.h"
+/**
+ * _print_rev_recursion - prints string in reverse
+ * @s : dtring to be reversed
+ *
+ * Return : returns nothing
+ */
+void _prints_rev_recursion(char *s)
+{
+	if (s != '\0')
+	{
+		_prints_rev_string(s++)
+		_putchar(s)
+	}
+	putchar('\n');
+
+		
