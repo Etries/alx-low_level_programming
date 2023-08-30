ffb82e0ad7790499582672bd3ffa930177f143d6
Etries <elyudmichael@gmail.com>
2023-08-30 15:33:41 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * sqr_eval - evaulates passible swuare roots
+ * @x: parameter one
+ * @y: parameter two
+ *
+ * Returns: on success 1 ans failure -1
+ */
+int sqr_eval(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y >x)
+		return (-1);
+	return sqr_eval(x, b + 1);
+}
+
+
+/**
+ *sqrt_recursion - returns natural square root of a number
+ *@n : number parameter
+ *
+ * Return: retunrs the sware root
+ */
+int _sqrt_recursion(int n)
+{
+	return(sqr_eval(n, 1);
+}
