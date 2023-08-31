f8321f0bf082899428935f811d29e70e59ab4dfc
Etries <elyudmichael@gmail.com>
2023-08-31 15:41:38 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,23 +11,24 @@ int last_index(char *s)
 
 	if (*s != '\0')
 		n += last_index(s + 1) + 1;
-	return (n );
+	return (n);
 }
 /**
  * checks_palindrome - checks if agiven substring are palindrome
  * @s: string parameter
  * @i: index of a char to be checked
  * @j: index of a char to be checked
+ * @pair: cheks if the length is even or edd
  * Return: return 1 if palindrome else 0
  */
 int checks_palindrome(char *s, int i, int j, int pair)
 {
-	if ((i == j && pair !=0) || (i == j + 1 && pair == 0))
+	if ((i == j && pair != 0) || (i == j + 1 && pair == 0))
 		return (1);
 	else if (s[i] != s[j])
 		return (0);
 	else
-		return(checks_palindrome(s, i + 1, j - 1, pair));
+		return (checks_palindrome(s, i + 1, j - 1, pair));
 }
 
 /**
