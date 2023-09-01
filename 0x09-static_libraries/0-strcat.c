db933eb6c3752d891a5da794b5ed5a7357f868be
Etries <elyudmichael@gmail.com>
2023-09-01 16:40:18 -0700

    README.md

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatnates two strings
+ * @dest : the destination string
+ * @src : the sorce to be appended to dest
+ *
+ * Return: return the pointer to the string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len;
+
+	len = 0;
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+	while (*src != '\0')
+	{
+		dest[len++] = *(src++);
+	}
+	dest[len++]  = '\0';
+	return (dest);
+}
---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character in a string
+ * @s : string to be searched
+ * @c : character to be searched
+ * Return: returns a pointer to a char in the string
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return (NULL);
+}
