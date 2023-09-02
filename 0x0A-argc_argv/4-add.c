223efe07172f24d389202a9ce8f2068ef839b053
Etries <elyudmichael@gmail.com>
2023-09-01 18:33:17 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,25 +6,29 @@
  *main - adds two positive integers
  *@argc : number or args
  *@argv : array of args
- *Return; returns o on succes and 1 on error
+ *Return: returns o on succes and 1 on error
  */
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    for (int i = 1; i < argc; i++) {
-        int j = 0;
-        while (argv[i][j] != '\0') {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-        int num = atoi(argv[i]);
-        if (num > 0) {
-            sum += num;
-        }
-    }
-    printf("%d\n", sum);
-    return 0;
-}
+int main(int argc, char *argv[])
+{
+	int sum, i, num, j;
 
+	sum  = 0;
+	for (i = 1; i < argc; i++)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		j++;
+		}
+		num = atoi(argv[i]);
+		if (num > 0)
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
