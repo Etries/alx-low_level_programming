f280780d20adb1a96ed3fcb408077e3a757a7c9d
Etries <elyudmichael@gmail.com>
2023-09-05 15:15:52 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,11 @@ char *create_array(unsigned int size, char c)
 	int i;
 	char *array;
 
+	if (size == 0)
+		return NULL;
 	array = malloc(n * sizeof(char));
+	if (array == NULL)
+		return NULL;
 	for (i = 0; i < size; i++)
 		array[i] = c;
 	return (array);
