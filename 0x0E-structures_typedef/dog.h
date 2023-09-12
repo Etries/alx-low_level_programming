ba1a4bd13f7c27997ef920fe88e0e9b11a4d6da2
Etries <elyudmichael@gmail.com>
2023-09-11 19:07:28 -0700

    intitial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*dog - a new type of struc data
+*@name: name of dog
+*@age: age of dog
+*@owner: nam of the ownere
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
