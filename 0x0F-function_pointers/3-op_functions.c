e3634ab797afc0d6682c801024d82420c3d09e20
Etries <elyudmichael@gmail.com>
2023-09-13 16:57:41 -0700

    3-main.c

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+/**
+ * op_add - Adds two integers
+ * @a: int parameter to be added
+ * @b: int parameter to be added
+ * Return: Returns sum of the integers
+**/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - subtracts two integers
+ * @a:first int parameter
+ * @b:second int parameter
+ * Return: Returns sunsbruction of the integers
+**/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Multiplies two integers
+ * @a: first int  param
+ * @b: 2nd int param
+ * Return: Returns multiplication of integers
+**/
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - Divides two integers
+ * @a: int param
+ * @b: int param
+ * Return: Returns result
+**/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Modules two numbers
+ * @a: int param 1
+ * @b: int param 2
+ * Return: Returns modules of the numbers
+**/
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
