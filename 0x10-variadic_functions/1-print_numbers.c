a4be96a4d3ae088cea542e6fa98e99771e72a70f
Etries <elyudmichael@gmail.com>
2023-09-14 04:28:52 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdargs.h>
+
+/**
+ * print_numbers - prints all numbers in args followed by
+ * a new line
+ * @separator: the string to be printed between numbers
+ * @n : number of args passed 
+ * Return : returns nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list vargs_list;
+	unsigned int i;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(valist);
+}
