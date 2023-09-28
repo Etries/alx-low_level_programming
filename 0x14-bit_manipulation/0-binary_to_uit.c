ab977b724db5094f4ffd51cdf6d24f93ad7059f4
Elyud <elyudmichael@gmail.com>
2023-09-28 15:20:39 +0000

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uit.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts a binary into a positive decimal
+ * @b: pointer to the binary(As a string)
+ *
+ * Return: returns a numbers or 0 if b is null
+ * or the are some non digits
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n, i, sum;
+	int base = 1;
+
+
+	i = 0;
+	sum = 0;
+	if (b == NULL)
+		return (0);
+	while (b[i] != '\0')
+	{
+		i++;
+	}
+	for (n = i - 1; n >= 0; n--)
+	{
+		if (b[n] != '0' || b[n] != '1')
+			return (0);
+		sum += (b[n] - 48) * (base);
+		base *= 2;
+	}
+	return (sum);
