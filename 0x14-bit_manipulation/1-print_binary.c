9251b71536a6f95b8d4fab5446372c31a930ee63
Elyud <elyudmichael@gmail.com>
2023-09-28 20:39:38 +0000

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,29 +8,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int x;
-	int flag = 0;
+if (n > 1)
+	print_binary(n >> 1);
 
-	x = 1UL << (sizeof(unsigned long int) * 8 - 1);
-	while (x > 0)
-	{
-		if ((n && x == 0 && flag == 0))
-		{
-			x >>= 1;
-			continue;
-		}
-
-		if ((n & x) != 0)
-		{
-			flag = 1;
-			_putchar('1');
-		}
-		else
-		{
-			_putchar('0');
-		}
-		x >>= 1;
-
-
-	}
+_putchar((n & 1) + '0');
 }
