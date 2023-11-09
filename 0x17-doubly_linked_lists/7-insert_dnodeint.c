3bfb5653c778429556669d4e47583d15b731d611
Elyud <elyudmichael@gmail.com>
2023-11-09 21:35:13 +0000

    betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -3,7 +3,7 @@
  *insert_dnodeint_at_index- inserts node at given index.
  *@h: a ptr to head of DLL
  *@idx: index for new node to be placed
- *@n: integer value 
+ *@n: integer value
  *Return: returns address of the new node or NULL if it failed.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
